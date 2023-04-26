9b68aa5388cf9582923fdd8f8fead68b77b058da
mensheng <feixiangmeng@outlook.com>
2023-04-26 19:03:21 +0800

    Add miniprogram base class.

---
+++ src/base/wxBase/app.ts
@@ -0,0 +1,49 @@
+import { BaseClass, InstanceBaseEvent, InstanceType } from "../base";
+
+type AppEventType = "onLaunch" 
+  | "onShow" 
+  | "onHide" 
+  | "onError"
+
+class AppBase extends BaseClass<AppEventType> {
+  constructor() {
+    super(InstanceType.App);
+  }
+
+  onLaunch = (() => {
+    const self = this;
+    return function (this: AppBase, options: WechatMiniprogram.LaunchOptionsApp) {
+      self.instance = this as any;
+      self.emit(InstanceBaseEvent.Lifecycle, "onLaunch", options);
+    }
+  })();
+
+  onShow = (() => {
+    const self = this;
+    return function (this: AppBase, options: WechatMiniprogram.LaunchOptionsApp) {
+      self.emit(InstanceBaseEvent.Lifecycle, "onShow", options);
+    }
+  })();
+
+  onHide = (() => {
+    const self = this;
+    return function (this: AppBase) {
+      self.emit(InstanceBaseEvent.Lifecycle, "onHide");
+    }
+  })();
+
+  onError = (() => {
+    const self = this;
+
+    return function(this: AppBase, error: string) {
+      self.emit(InstanceBaseEvent.Lifecycle, "onError", error);
+    }
+  })();
+}
+
+interface IApp {
+  new (): AppBase & WechatMiniprogram.App.Option;
+  prototype: AppBase;
+}
+
+export const AppBaseClass = AppBase as any as IApp;
