a8b64157648e7062d12d3a9dd1c08c0a17d8af33
mensheng <feixiangmeng@outlook.com>
2023-04-28 14:53:18 +0800

    Update types file.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,14 @@
 
+type TODOType = any;
+
 interface MessageObject {
   data: {
     type: string,
     value: any
   }
+}
+
+declare module "wasm/*" {
+  let Module: any;
+  export default Module;
 }
