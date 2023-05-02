81bd4021b15acf8a311577f41de3ad20034054bc
mensheng <feixiangmeng@outlook.com>
2023-05-02 14:11:36 +0800

    Add missing types.

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,3 +12,22 @@ declare module "wasm/*" {
   let Module: any;
   export default Module;
 }
+
+declare module "css/*" {
+  
+}
+
+/** @todo 待完善 */
+declare module "weui.js" {
+  var loading: any;
+}
+
+declare module "mp-webpack-plugin" {
+  const _: any;
+  export default _;
+}
+
+declare module "terser-webpack-plugin" {
+  const _: any;
+  export default _;
+}
