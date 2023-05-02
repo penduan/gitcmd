865ffd054d6be62fa3e8ee1c0e4231f6caab61b5
mensheng <feixiangmeng@outlook.com>
2023-05-02 14:10:27 +0800

    Refactor webpack config file use typescript.

---
+++ build/webpack.mp.config.ts
@@ -0,0 +1,87 @@
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CssMiniPlugin from "css-minimizer-webpack-plugin";
+import TerserPlugin from "terser-webpack-plugin";
+import MpPlugin from "mp-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import config from "./config";
+import { merge } from 'webpack-merge';
+
+const isOptimize = config.isDev; // 是否压缩业务代码，开发者工具可能无法完美支持业务代码使用到的 es 特性，建议自己做代码压缩
+
+const mpCustomConfig = {
+
+}
+
+const mpBaseConfig = {
+  entry: config.mp.entry,
+  output: config.mp.output,
+  mode: 'production',
+  optimization: {
+    runtimeChunk: false, // 必需字段，不能修改
+    splitChunks: {
+      // 代码分隔配置，不建议修改
+      chunks: 'all',
+      minSize: 1000,
+      minChunks: 1,
+      maxAsyncRequests: 100,
+      maxInitialRequests: 100,
+      automaticNameDelimiter: '~',
+      name: false,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+
+    minimizer: isOptimize
+      ? [
+          // 压缩CSS
+          new CssMiniPlugin({
+            test: /\.(css|wxss)$/g,
+            minimizerOptions: {
+              preset: [
+                'default',
+                {
+                  discardComments: {
+                    removeAll: true,
+                  },
+                  minifySelectors: false, // 因为 wxss 编译器不支持 .some>:first-child 这样格式的代码，所以暂时禁掉这个
+                },
+              ],
+            },
+          }),
+          // 压缩 js
+          new TerserPlugin({
+            test: /\.js(\?.*)?$/i,
+            parallel: true,
+          }),
+        ]
+      : [],
+  },
+  target: 'web', // 必需字段，不能修改
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].wxss' }),
+    new MpPlugin(config.mp.mpPlugin),
+    new CopyPlugin({patterns: config.mp.copy}),
+    new webpack.DefinePlugin(config.mp.defines)
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  }
+}
+
+// @ts-ignore
+export default merge(mpBaseConfig, config.commonWebpackConfig, mpCustomConfig);
---
+++ build/webpack.web.config.ts
@@ -0,0 +1,153 @@
+import webpack from 'webpack';
+import { merge } from 'webpack-merge';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import portfinder from 'portfinder';
+import CopyPlugin from 'copy-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import CssMiniPlugin from "css-minimizer-webpack-plugin";
+import config, { getAbsPath } from './config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
+
+const LOCALHOST_PORT = +(process.env.PORT || "") || 8080;
+
+const htmlPluginList = config.isDev 
+  ? Object.keys(config.web.entry).map(name => {
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: 'index.html',
+      inject: true,
+      chunks: [name],
+    })
+  }) 
+  : Object.keys(config.web.entry).map(name => {
+    return new HtmlWebpackPlugin({
+      filename: getAbsPath(`./dist/web/${name}.html`),
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+      },
+      chunks: [name],
+    });
+  });
+
+const webBaseConfig:webpack.Configuration & {devServer?: any} = {
+  mode: config.isDev ? "development" : "production",
+  devtool: config.isDev ? "cheap-module-source-map" : false,
+  entry: config.web.entry,
+  output: config.web.output,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // @ts-ignore
+        use: [ config.isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin(config.web.defines),
+    ...htmlPluginList,
+    new CopyPlugin({ patterns: [...config.web.copy] }),
+  ]
+}
+
+if (config.isDev) { // Developement mode.
+  console.log("development");
+  webBaseConfig.devServer = {
+    historyApiFallback: {
+      rewrites: [{from: /.*/, to: '/index.html'}],
+    },
+    hot: true,
+    compress: true,
+    host: process.env.HOST || 'localhost',
+    port: LOCALHOST_PORT,
+    open: true, // 自动打开浏览器
+    client: {
+      logging: 'warn',
+      overlay: { // 展示全屏报错
+        warnings: false,
+        errors: true
+      },
+    },
+    static: {
+      publicPath: '/',
+    },
+    proxy: {},
+  }
+  webBaseConfig.watchOptions = {
+    poll: false,
+  }
+  webBaseConfig.plugins!.unshift(new webpack.HotModuleReplacementPlugin());
+
+} else { // Production mode.
+  console.log("production");
+  webBaseConfig.optimization = {
+    splitChunks: {
+      // 代码分割配置
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: false,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    minimizer: [
+      // 压缩CSS
+      new CssMiniPlugin({
+        test: /\.css$/g,
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: {
+                removeAll: true,
+              },
+            },
+          ],
+        }
+      }),
+      // 压缩 js
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        parallel: true,
+      }),
+    ],
+  }
+  webBaseConfig.plugins!.push(new MiniCssExtractPlugin({ filename: path.posix.join('static', 'css/[name].[hash].css') }));
+  webBaseConfig.plugins!.push(new webpack.ids.HashedModuleIdsPlugin())
+}
+
+// @ts-ignore
+const webpackConfig = merge(webBaseConfig, config.commonWebpackConfig);
+
+export default config.isDev 
+  ? new Promise((resolve, reject) => {
+    portfinder.basePort = LOCALHOST_PORT;
+    portfinder.getPort((err, port) => {
+      if (err) {
+        reject(err)
+      } else {
+        // @ts-ignore
+        webpackConfig.devServer.port = port;
+
+        resolve(webpackConfig);
+      }
+    });
+  }) 
+  : webpackConfig;
